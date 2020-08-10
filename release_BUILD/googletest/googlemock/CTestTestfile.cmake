760eedc19177d9c3f72ff3c062441077f680a096
mgood7123 <smallville7123@gmail.com>
2020-08-10 14:07:00 +1000

    remove release build

--- release_BUILD/googletest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/smallville7123/AndroidCompositor/app/src/main/jni/GLIS/dependancies/AnyOpt/googletest/googlemock
-# Build directory: /home/smallville7123/AndroidCompositor/app/src/main/jni/GLIS/dependancies/AnyOpt/release_BUILD/googletest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
