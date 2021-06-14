220e5b1d3eca33e7e7efe815cefe3d843aa4466a
mgood7123 <smallville7123@gmail.com>
2021-06-15 04:02:18 +1000

    Delete debug_BUILD directory

--- debug_BUILD/googletest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/smallville7123/Desktop/CircularBuffer/app/src/main/cpp/CircularBuffer/AnyOpt/googletest
-# Build directory: /Users/smallville7123/Desktop/CircularBuffer/app/src/main/cpp/CircularBuffer/AnyOpt/debug_BUILD/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
